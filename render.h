0b667b64d4020d2b758eb8d53dd81fa78882eedc
Soham Borate <soham.borate1@gmail.com>
2025-02-16 20:34:23 +0530

    typo

--- render.h
+++ render.h
@@ -4,5 +4,3 @@
 void render_scene(vec3 *origin, vec3 *sun, sphere *ball, int HEIGHT, int WIDTH, RGB pixels[HEIGHT][WIDTH]);
 
 RGB render_pixel(vec3 *origin, vec3 *sun, sphere *ball, const int ROW, const int COLUMN, const int HEIGHT, const int WIDTH);
-
-float get_point_from_sphere(vec3 *origin, vec3 *direction, sphere *ball);
